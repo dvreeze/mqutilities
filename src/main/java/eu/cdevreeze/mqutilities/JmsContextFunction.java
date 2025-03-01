850a65b2d7103da3dd023801a14dfc219e9f8070
Chris de Vreeze <chris.de.vreeze@caiway.net>
2025-03-01 10:53:56 +0100

    Preparing for big refactoring

---
+++ src/main/java/eu/cdevreeze/mqutilities/JmsContextFunction.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2025-2025 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.mqutilities;
+
+import jakarta.jms.JMSContext;
+
+import java.util.function.Function;
+
+/**
+ * {@link java.util.function.Function} taking a {@link jakarta.jms.JMSContext}.
+ *
+ * @author Chris de Vreeze
+ */
+public interface JmsContextFunction<T> extends Function<JMSContext, T> {
+}
---
+++ src/main/java/eu/cdevreeze/mqutilities/JmsContextToElementFunctionFactory.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2025-2025 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.mqutilities;
+
+import eu.cdevreeze.yaidom4j.dom.immutabledom.Element;
+
+import java.util.List;
+
+/**
+ * {@link JmsContextFunctionFactory} creating {@link JmsContextToElementFunction} instances.
+ *
+ * @author Chris de Vreeze
+ */
+public interface JmsContextToElementFunctionFactory extends JmsContextFunctionFactory<Element> {
+
+    @Override
+    JmsContextToElementFunction apply(List<String> args);
+}
