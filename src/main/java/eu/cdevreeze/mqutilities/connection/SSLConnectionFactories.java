8bdf3c87a9eee17ef12a2c1c575b94221f68aa2a
Chris de Vreeze <chris.de.vreeze@caiway.net>
2025-03-03 22:49:52 +0100

    More MP-Config, but not yet config interacting with CDI

--- src/main/java/eu/cdevreeze/mqutilities/connection/SSLConnectionFactories.java
+++ src/main/java/eu/cdevreeze/mqutilities/connection/SSLConnectionFactories.java
@@ -16,23 +16,70 @@
 
 package eu.cdevreeze.mqutilities.connection;
 
+import com.ibm.msg.client.jakarta.jms.JmsConnectionFactory;
+import com.ibm.msg.client.jakarta.jms.JmsFactoryFactory;
+import com.ibm.msg.client.jakarta.wmq.WMQConstants;
+import com.ibm.msg.client.jakarta.wmq.common.CommonConstants;
 import eu.cdevreeze.mqutilities.qualifier.connection.ConnectionType;
 import eu.cdevreeze.mqutilities.qualifier.connection.HasConnectionType;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSException;
+import jakarta.jms.JMSRuntimeException;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+import java.util.Objects;
 
 /**
- * CDI-injectable SSL-enabled JMS {@link ConnectionFactory}.
+ * CDI-injectable SSL-enabled JMS {@link ConnectionFactory}, using 2-way SSL.
  *
  * @author Chris de Vreeze
  */
+@ApplicationScoped
 public class SSLConnectionFactories {
 
     @Produces
     @HasConnectionType(ConnectionType.TWO_WAY_SSL)
     @ApplicationScoped
     public ConnectionFactory getConnectionFactory() {
-        return new SSLConnectionFactorySupplier().get();
+        // Ugly!
+        Config config = ConfigProvider.getConfig();
+
+        try {
+            String jakartaWmqProviderName = WMQConstants.JAKARTA_WMQ_PROVIDER;
+            JmsFactoryFactory jmsFactoryFactory = JmsFactoryFactory.getInstance(jakartaWmqProviderName);
+
+            JmsConnectionFactory cf = jmsFactoryFactory.createConnectionFactory();
+
+            cf.setStringProperty(CommonConstants.WMQ_HOST_NAME, config.getValue("two_way_ssl.wmq_host_name", String.class));
+            cf.setIntProperty(CommonConstants.WMQ_PORT, config.getValue("two_way_ssl.wmq_port", Integer.class));
+            cf.setStringProperty(CommonConstants.WMQ_CHANNEL, config.getValue("two_way_ssl.wmq_channel", String.class));
+            cf.setIntProperty(CommonConstants.WMQ_CONNECTION_MODE, config.getValue("two_way_ssl.wmq_connection_mode", Integer.class));
+            cf.setStringProperty(CommonConstants.WMQ_QUEUE_MANAGER, config.getValue("two_way_ssl.wmq_queue_manager", String.class));
+            cf.setStringProperty(CommonConstants.WMQ_SSL_CIPHER_SUITE, config.getValue("two_way_ssl.wmq_ssl_cipher_suite", String.class));
+
+            Objects.requireNonNull(
+                    System.getProperty("javax.net.ssl.keyStore"),
+                    "Missing system property 'javax.net.ssl.keyStore'"
+            );
+            Objects.requireNonNull(
+                    System.getProperty("javax.net.ssl.keyStorePassword"),
+                    "Missing system property 'javax.net.ssl.keyStorePassword'"
+            );
+            Objects.requireNonNull(
+                    System.getProperty("javax.net.ssl.trustStore"),
+                    "Missing system property 'javax.net.ssl.trustStore'"
+            );
+            Objects.requireNonNull(
+                    System.getProperty("javax.net.ssl.trustStorePassword"),
+                    "Missing system property 'javax.net.ssl.trustStorePassword'"
+            );
+
+            return cf;
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage(), e.getErrorCode(), e);
+        }
     }
 }
--- src/main/java/eu/cdevreeze/mqutilities/connection/SimpleConnectionFactories.java
+++ src/main/java/eu/cdevreeze/mqutilities/connection/SimpleConnectionFactories.java
@@ -16,14 +16,24 @@
 
 package eu.cdevreeze.mqutilities.connection;
 
+import com.ibm.msg.client.jakarta.jms.JmsConnectionFactory;
+import com.ibm.msg.client.jakarta.jms.JmsFactoryFactory;
+import com.ibm.msg.client.jakarta.wmq.WMQConstants;
+import com.ibm.msg.client.jakarta.wmq.common.CommonConstants;
 import eu.cdevreeze.mqutilities.qualifier.connection.ConnectionType;
 import eu.cdevreeze.mqutilities.qualifier.connection.HasConnectionType;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSException;
+import jakarta.jms.JMSRuntimeException;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 /**
- * CDI-injectable "simple" JMS {@link ConnectionFactory}.
+ * CDI-injectable "simple" JMS {@link ConnectionFactory}, using password authentication.
+ * The configuration properties default to the values for the Dockerized MQ queue manager
+ * that can be used for training purposes.
  *
  * @author Chris de Vreeze
  */
@@ -34,6 +44,27 @@ public class SimpleConnectionFactories {
     @HasConnectionType(ConnectionType.SIMPLE)
     @ApplicationScoped
     public ConnectionFactory getConnectionFactory() {
-        return new SimpleConnectionFactorySupplier().get();
+        // Ugly!
+        Config config = ConfigProvider.getConfig();
+
+        try {
+            String jakartaWmqProviderName = WMQConstants.JAKARTA_WMQ_PROVIDER;
+            JmsFactoryFactory jmsFactoryFactory = JmsFactoryFactory.getInstance(jakartaWmqProviderName);
+
+            JmsConnectionFactory cf = jmsFactoryFactory.createConnectionFactory();
+
+            cf.setStringProperty(CommonConstants.WMQ_HOST_NAME, config.getValue("simple.wmq_host_name", String.class));
+            cf.setIntProperty(CommonConstants.WMQ_PORT, config.getValue("simple.wmq_port", Integer.class));
+            cf.setStringProperty(CommonConstants.WMQ_CHANNEL, config.getValue("simple.wmq_channel", String.class));
+            cf.setIntProperty(CommonConstants.WMQ_CONNECTION_MODE, config.getValue("simple.wmq_connection_mode", Integer.class));
+            cf.setStringProperty(CommonConstants.WMQ_QUEUE_MANAGER, config.getValue("simple.wmq_queue_manager", String.class));
+            cf.setBooleanProperty(CommonConstants.USER_AUTHENTICATION_MQCSP, config.getValue("simple.user_authentication_mqcsp", Boolean.class));
+            cf.setStringProperty(CommonConstants.USERID, config.getValue("simple.userid", String.class));
+            cf.setStringProperty(CommonConstants.PASSWORD, config.getValue("simple.password", String.class));
+
+            return cf;
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage(), e.getErrorCode(), e);
+        }
     }
 }
